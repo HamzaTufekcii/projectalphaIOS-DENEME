12e56cba7b1c9c563c6c949b1f5706d715f985d3
Hamza Tüfekçi <hamzatufekci1234@gmail.com>
2025-08-12 02:25:30 +0300

    yes

---
+++ IOS/IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Hamza Tüfekçi on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS/IOSTests/IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTests.swift
+//  IOSTests
+//
+//  Created by Hamza Tüfekçi on 12.08.2025.
+//
+
+import Testing
+@testable import IOS
+
+struct IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
