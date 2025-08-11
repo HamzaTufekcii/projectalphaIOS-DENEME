a85f0c722d21815882dd6698b74aa75f867fd08f
Hamz <hamzatufekci1234@gmail.com>
2025-08-11 18:23:34 +0300

    as

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-  export default defineConfig({
-    plugins: [react()],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8080',
-          changeOrigin: true,
-        },
-      }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://192.168.1.20:8443',
+        changeOrigin: true,
+        secure: false  // self-signed için şart
+      },
     },
-  });
+  },
+});
