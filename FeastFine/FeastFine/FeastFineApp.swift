0eead0a0593ff2c72f911c3300e15557498b1402
Hamza Tüfekçi <hamzatufekci1234@gmail.com>
2025-08-12 02:12:20 +0300

    yes

---
+++ FeastFine/FeastFine/FeastFineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeastFineApp.swift
+//  FeastFine
+//
+//  Created by Hamza Tüfekçi on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FeastFineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FeastFine/FeastFineTests/FeastFineTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeastFineTests.swift
+//  FeastFineTests
+//
+//  Created by Hamza Tüfekçi on 12.08.2025.
+//
+
+import Testing
+@testable import FeastFine
+
+struct FeastFineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
