49c60372b30bff0e02e8d5eaacff2858d4744f4f
Hamz <hamzatufekci1234@gmail.com>
2025-08-15 21:02:36 +0300

    bnhfbvn

--- frontend/src/services/businessService.js
+++ frontend/src/services/businessService.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE = '/api/business';
+const API_BASE = 'api/business';
 
 /**
  * Fetches all businesses.
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/users';
+const API_URL = 'api/users';
 
 /**
  * save user data to local storage
